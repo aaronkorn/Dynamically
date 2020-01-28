b911233f63b1ca4509f8454498f28a7196807fd2
aaronkorn <aaronkorn@gmail.com>
2020-01-28 10:46:43 -0800

    Added a layout to the body

--- Dynamically/ContentView.swift
+++ Dynamically/ContentView.swift
@@ -18,12 +18,40 @@ struct ContentView: View {
   @State private var lastNameFilter = "A"
   
   var body: some View {
-    Text("Hello, World!")
-  }
-}
+    VStack {
+      // list of matching singers
+      
+      Button("Add Examples") {
+        let taylor = Singer(context: self.moc)
+        taylor.firstName = "Taylor"
+        taylor.lastName = "Swift"
+        
+        let ed = Singer(context: self.moc)
+        ed.firstName = "Ed"
+        ed.lastName = "Sheeran"
+        
+        let adele = Singer(context: self.moc)
+        adele.firstName = "Adele"
+        adele.lastName = "Adkins"
+        
+        try? self.moc.save()
+      }//Button
+      
+      Button("Show A") {
+        self.lastNameFilter = "A"
+      }//Button
+      
+      Button("Show S") {
+        self.lastNameFilter = "S"
+      }//Button
+      
+    }//VStack
+  }//body
+  
+}//ContentView
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
   }
-}
+}//ContentView_Previews
